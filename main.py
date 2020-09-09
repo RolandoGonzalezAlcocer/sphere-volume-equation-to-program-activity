eb338f4dbbe8609b6de193a9016e0f8686d4c451
RolandoGonzalezAlcocer <70342390+RolandoGonzalezAlcocer@users.noreply.github.com>
2020-09-09 14:02:59 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,4 @@
+radius = 4
+Volume = (4*Math.PI*(radius**3))/3
+
+basic.show_number(Volume) 
---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(Volume)
+})
+let Volume = 0
+let radius = 4
+let pi = 3.14159
+Volume = 4 * pi * radius ** 3 / 3
